d698a3b0fa15db28947f8f2f35efc0e58431b01d
sese0002 <sese0002@stud.kea.dk>
2025-12-18 12:02:16 +0100

    sera_forside_torsdag_rettelser

--- events.js
+++ events.js
@@ -1,7 +1,9 @@
 console.log("loaded...");
+/*Scriptet er blevet indlæst*/
 
 const eventsUrl = "https://qpxfpzldrliwvmichmrh.supabase.co/rest/v1/Events";
 const key = "sb_publishable_SEZpXmVn1FJkomllatBYGA_n-d_5JTH";
+/*Vi giver adgang via "key" til at hnete supabase url, og taler til tabellen "Events"*/
 
 console.log("min url", eventsUrl);
 
@@ -10,8 +12,10 @@ const options = {
     apikey: key,
   },
 };
+/*API nøglen sendes med i requesten, så Supabase tillader adgang*/
 
 getData(eventsUrl, options);
+/*Starter funktionen der henter dataen*/
 
 function getData(url) {
   fetch(url, options).then((res) =>
@@ -21,20 +25,26 @@ function getData(url) {
     })
   );
 }
+/*Fetch laver et kald til Supabase*/
+/*JSon betyder det er konverteret til dette */
+/*"showevents" betyder at dataen sendes videre til funktionen, og den vises på siden*/
+/*ID bruges til at vise det rigtige event*/
 
 function showEvents(events) {
   const eventsListContainer = document.querySelector("#events");
-  eventsListContainer.innerHTML = "";
+  eventsListContainer.innerHTML = ""; /*rydder indholder, så det kan gentages*/
   console.log("events", events);
   events.forEach((event) => {
+    /*gennemgår hver event fra databasen*/
     eventsListContainer.innerHTML += `
-    <a href="produkt.html?id=${event.id}">
+    <a href="produkt.html?id=${event.id}"> 
        <article class="events">
         <div class="event-produkt">
         <img src="${event.Img}">
           <h3 class="h3_events">${event.Overskrift}</h3>
           <h4 class="h4_events">${event.Dato}</h4>
           <p class="p_events">${event.Eventinfo}</p>
+          <p class="p_events"> Read more... </p>
         </div>
       </article> 
       </a>
